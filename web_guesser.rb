eda4467ba6131d73f4c9235add1d8cc0e675fe82
Nicholas Erhardt <ski.climb.europe@gmail.com>
2016-11-19 12:13:26 -0700

    Extension: colorize output

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,49 +5,67 @@ message = ""
 set :secret_number, rand(100) + 1
 
 def set_message(guess)
-  return correct(guess) if correct?(guess)
-  return too_high(guess) if too_high?(guess)
-  return too_low(guess) if too_low?(guess)
+  return correct      if correct?(guess)
+  return too_high     if too_high?(guess)
+  return way_too_high if way_too_high?(guess)
+  return too_low      if too_low?(guess)
+  return way_too_low  if way_too_low?(guess)
 end
 
-def too_high(guess)
-  if guess > settings.secret_number + 5
-    "Way too high!"
-  else
-    "Too high!"
-  end
+def set_color(guess)
+  return "crimson"    if ( way_too_low?(guess) || way_too_high?(guess) )
+  return "lightcoral" if (     too_low?(guess) ||     too_high?(guess) )
+  return "lightgreen" if       correct?(guess)
+end
+
+def correct
+  "You got it right!<br />The SECRET NUMBER is #{settings.secret_number}"
+end
+
+def way_too_high
+  "Way too high!"
+end
+
+def way_too_low
+  "Way too low!"
+end
+
+def too_high
+  "Too high!"
+end
+
+def too_low
+  "Too low!"
+end
+
+def way_too_high?(guess)
+  guess > settings.secret_number + 5
 end
 
 def too_high?(guess)
-  guess > settings.secret_number
+  guess > settings.secret_number && guess <= settings.secret_number + 5
 end
 
-def too_low(guess)
-  if guess < settings.secret_number - 5
-    "Way too low!"
-  else
-    "Too low!"
-  end
+def way_too_low?(guess)
+  guess < settings.secret_number - 5
 end
 
 def too_low?(guess)
-  guess < settings.secret_number
+  guess < settings.secret_number && guess >= settings.secret_number - 5
 end
 
 def correct?(guess)
   guess == settings.secret_number
 end
 
-def correct(guess)
-  "You got it right!<br />The SECRET NUMBER is #{settings.secret_number}"
-end
-
 get '/' do
   guess = params['guess'].to_i
   message = set_message(guess)
+  color = set_color(guess)
   erb :index, locals: {
                         number: settings.secret_number,
                         guess: guess,
-                        message: message
+                        message: message,
+                        color: color
                       }
 end
